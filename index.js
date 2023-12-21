fc48b1f47b29a001463ffca92232a230b4e2b6e0
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-12-21 10:52:50 +0000

    apply styling to the calculator peoperties

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const display = document.getElementById("screen");
 
+// Dispalys the input in the screen
 function displays(input) {
     display.value += input;
 }
@@ -8,11 +9,12 @@ function calculate() {
     try {
         display.value = eval(display.value);
     } catch (error) {
+        // Catches the error for unknown formula
         display.value = "ERROR";
     }
 }
 
-
+// Clears the screen from values
 function clears() {
     display.value = "";
 }
